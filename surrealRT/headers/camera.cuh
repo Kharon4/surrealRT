907e111b5d03d403b04f018d874f292c6eee0757
Abhishek Khurana <abhishekkhurana12345@gmail.com>
2020-04-12 17:13:43 +0530

    added camera class

---
+++ surrealRT/headers/camera.cuh
@@ -0,0 +1,17 @@
+#pragma once
+#include "vec3.cuh"
+
+class screen {
+public:
+	vec3d screenCenter, halfLeft, halfUp;
+	short resX, resY;
+
+	screen(short xRes = 800, short yRes = 600, vec3d ScCenter = vec3d(0, 0, 0), vec3d hfLeft = vec3d(1, 0, 0), vec3d hfUp = vec3d(0, 0, 1));
+};
+
+class camera {
+public:
+	screen sc;
+	vec3d vertex;
+	camera(vec3d Vertex, short xRes, short yRes, vec3d ScCenter, vec3d hfLeft, vec3d hfUp);
+};
---
+++ surrealRT/src/camera.cu
@@ -0,0 +1,15 @@
+#include "camera.cuh"
+
+screen::screen(short xRes, short yRes, vec3d ScCenter, vec3d hfLeft, vec3d hfUp) {
+	resX = xRes;
+	resY = yRes;
+	screenCenter = ScCenter;
+	halfLeft = hfLeft;
+	halfUp = hfUp;
+}
+
+
+camera::camera(vec3d Vertex ,short xRes, short yRes, vec3d ScCenter, vec3d hfLeft, vec3d hfUp) {
+	vertex = Vertex;
+	sc = screen(xRes, yRes, ScCenter, hfLeft, hfUp);
+}
